585021afabc3478d0b3591425819c79f18a9b9ee
Konstantin Eppinger <koneppinger@gmail.com>
2021-11-10 12:41:54 +0100

    Added Chart.js

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,59 @@
 <script>
+	// import { onMount } from 'svelte';
+	import moment from 'moment';
 	import { city, weather } from '../stores/weatherstore';
-	// $: temp = Math.round($weather.main.temp);
-	// $: humidity = $weather.main.humidity;
-	// $: pressure = $weather.main.pressure;
+	import Chart from '../compontents/chart.svelte';
+
+	let chartsReady = false;
+	let mounted = false;
+
+	// onMount(() => {
+	// 	mounted = true;
+	// 	if (chartsReady) loadChartElements();
+	// });
+
+	// const chartsLoaded = () => {
+	// 	chartsReady = true;
+	// 	if (mounted) loadChartElements();
+	// };
+
+	let interimCity = $city;
+	let local_time = moment().format('MMM DD, HH:mma');
+	$: location = `${$weather.name}, ${$weather.country}`;
+	$: temp = Math.round($weather.current?.temp);
+	$: humidity = $weather.current?.humidity;
+	$: pressure = $weather.current?.pressure;
+	$: feels_like = Math.round($weather.current?.feels_like);
+	$: description = $weather.current?.weather[0]?.description;
+
+	// const loadChartElements = () => {};
 
 	const onKeyDown = (event) => {
 		if (event.keyCode !== 13) return;
+		$city = interimCity;
 	};
 
-	let promise = $weather;
-	console.log(promise);
-	// promise.then((data) => console.log(data.json()));
+	console.log($weather);
 </script>
 
 <svelte:head>
 	<title>Start - Open Weather Map with Svelte</title>
+	<!-- <script src="scripts/weatherchart.js" on:load={chartsLoaded}></script> -->
 </svelte:head>
 
 <section class="mx-auto max-w-xl px-8">
 	<label class="block mb-2" for="targetNumber">Ort</label>
-	<input class="rounded w-full" type="text" bind:value={$city} on:keydown={onKeyDown} />
+	<input class="rounded w-full" type="text" bind:value={interimCity} on:keydown={onKeyDown} />
 </section>
 <hr class="mt-10 mb-3 border-gray-300" />
 <section class="mx-auto max-w-xl px-8">
-	{#await promise then weather}
-		<p>{weather}</p>
-	{/await}
-	<!-- <p>Temperature: {temp}</p>
-	<p>Humidity: {humidity}</p>
-	<p>Pressure: {pressure}</p> -->
+	<p>{local_time}</p>
+	<p>{location}</p>
+	<p>Temperature: {temp}°C</p>
+	<p>Feels like {feels_like}°C. {description}.</p>
+	<p>Humidity: {humidity}%</p>
+	<p>Pressure: {pressure}hPa</p>
+</section>
+<section class="mx-auto max-w-xl px-8">
+	<Chart />
 </section>
--- src/stores/weatherstore.js
+++ src/stores/weatherstore.js
@@ -1,6 +1,19 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/env';
 
+export const weather = writable({});
+const fetchWeather = async (geocode) => {
+	if (Object.keys(geocode).length === 0) return;
+	const apiKey = `420cf3bc48a83bc3beed2e87e4735ec6`;
+	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${geocode.lat}&lon=${geocode.lon}&units=metric&exclude=hourly,minutely,alerts&appid=${apiKey}`;
+	const res = await fetch(url);
+	const data = await res.json();
+	const interimObject = { name: geocode.name, country: geocode.country };
+	const loadedWeather = { ...data, ...interimObject };
+	weather.set(loadedWeather);
+};
+
+export const geocode = writable({});
 const fetchGeocode = async (cityName) => {
 	if (!cityName) return;
 	const apiKey = `420cf3bc48a83bc3beed2e87e4735ec6`;
@@ -9,28 +22,21 @@ const fetchGeocode = async (cityName) => {
 	const data = await res.json();
 	const loadedGeocode = data.map((data) => {
 		return {
+			name: data.name,
+			country: data.country,
 			lat: data.lat,
 			lon: data.lon
 		};
 	});
-	return loadedGeocode[0];
+	geocode.set(loadedGeocode[0]);
 };
-
-export const weather = writable({});
-const fetchWeather = async (cityName) => {
-	if (!cityName) return;
-	fetchGeocode(cityName)
-		.then((response) => response)
-		.then((data) => {
-			const apiKey = `420cf3bc48a83bc3beed2e87e4735ec6`;
-			const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&units=metric&lang=de&appid=${apiKey}`;
-			const res = fetch(url);
-			weather.set(res);
-		});
-};
+geocode.subscribe((val) => {
+	if (Object.keys(val).length === 0) return;
+	fetchWeather(val);
+});
 
 export const city = writable(browser && (sessionStorage.getItem('city') || 'Paderborn'));
 city.subscribe((val) => {
 	browser && sessionStorage.setItem('city', val);
-	fetchWeather(val);
+	fetchGeocode(val);
 });
